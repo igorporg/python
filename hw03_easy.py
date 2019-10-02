da63e55dfc547e6f7971a186ed6f40c509babfc3
igorprog <igorprog@yandex.ru>
2019-10-02 21:45:38 +0300

    easy done

--- hw03_easy.py
+++ hw03_easy.py
@@ -34,37 +34,50 @@ print("HOMEWORK 3 Easy")
 while 1:
     print("Задача-1")
     print("Задача-2")
-    print("Задача-3")
     do = input("Введите номер задачи (0 - Выход): ")
 
-    # Задание-1:
-    # Напишите функцию, округляющую полученное произвольное десятичное число
-    # до кол-ва знаков (кол-во знаков передается вторым аргументом).
-    # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
-    # Для решения задачи не используйте встроенные функции и функции из модуля math.
     if do == "1":
         print("Задача-1")
         def my_round(number, ndigits):
+            if number is float and ndigits is int:
+                list = str(number).split(".")
+                celoe = int(list[0])
+                drobnoe = int(list[1][:ndigits+1])
+                ostatok = float("0." + list[1][ndigits + 1:])
+                if ostatok > 0.5:
+                    drobnoe += 1
+                    if len(str(drobnoe))> 6:
+                        celoe += 1
+                        drobnoe = 0
+            else:
+                return False
+            return float(str(celoe) + "." + str(drobnoe))
 
-            return number
-
-        print(my_round(0.6, 5))
-        print(my_round(0.4, 5))
-        print(my_round(0.9999967, 5))
+        print(my_round(2.1234567, 5))
+        print(my_round(2.1999967, 5))
+        print(my_round(2.9999967, 5))
+        print(my_round(5.9999997, 5))
         print("********************")
 
-    # Задача-3
-    if do == "3":
-        print("Задача-3")
-        spisok = [2,3,4,8,20,17]
-        i = 0
-        for num in spisok:
-            if num % 2 == 0:
-                spisok[i] /= 4
+    # Задача-2
+    if do == "2":
+        print("Задача-2")
+        def lucky_ticket(ticket_number):
+            if len(str(ticket_number)) == 6:
+                n1 = str(ticket_number)[:3]
+                n2 = str(ticket_number)[3:]
+                sum_n1 = int(n1[0]) + int(n1[1]) + int(n1[2])
+                sum_n2 = int(n2[0]) + int(n2[1]) + int(n2[2])
+                if sum_n1 == sum_n2:
+                    return True
+                else:
+                    return False
             else:
-                spisok[i] *= 2
-            i += 1
-        print(spisok)
+                return False
+
+        print(lucky_ticket(123006))
+        print(lucky_ticket(12321))
+        print(lucky_ticket(436751))
         print("********************")
 
     if do == "0":
