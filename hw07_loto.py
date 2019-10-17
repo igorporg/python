c7abc26b49c7593d43596597bcded1dbe604358b
igorprog <igorprog@yandex.ru>
2019-10-17 18:59:16 +0300

    Начал делать, создал две карточки

--- hw07_loto.py
+++ hw07_loto.py
@@ -1,93 +1,98 @@
 _author__ = 'Igor A.Provorov'
 
-# Задание-1:
-# Реализуйте описаную ниже задачу, используя парадигмы ООП:
-# В школе есть Классы(5А, 7Б и т.д.), в которых учатся Ученики.
-# У каждого ученика есть два Родителя(мама и папа).
-# Также в школе преподают Учителя. Один учитель может преподавать 
-# в неограниченном кол-ве классов свой определенный предмет. 
-# Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
-# но больше математику не может преподавать никто другой.
+"""
+== Лото ==
+Правила игры в лото.
+Игра ведется с помощью специальных карточек, на которых отмечены числа, 
+и фишек (бочонков) с цифрами.
+Количество бочонков — 90 штук (с цифрами от 1 до 90).
+Каждая карточка содержит 3 строки по 9 клеток. В каждой строке по 5 случайных цифр, 
+расположенных по возрастанию. Все цифры в карточке уникальны. Пример карточки:
+--------------------------
+    9 43 62          74 90
+ 2    27    75 78    82
+   41 56 63     76      86 
+--------------------------
+В игре 2 игрока: пользователь и компьютер. Каждому в начале выдается 
+случайная карточка. 
+Каждый ход выбирается один случайный бочонок и выводится на экран.
+Также выводятся карточка игрока и карточка компьютера.
+Пользователю предлагается зачеркнуть цифру на карточке или продолжить.
+Если игрок выбрал "зачеркнуть":
+	Если цифра есть на карточке - она зачеркивается и игра продолжается.
+	Если цифры на карточке нет - игрок проигрывает и игра завершается.
+Если игрок выбрал "продолжить":
+	Если цифра есть на карточке - игрок проигрывает и игра завершается.
+	Если цифры на карточке нет - игра продолжается.
 
-# Выбранная и заполненная данными структура должна решать следующие задачи:
-# 1. Получить полный список всех классов школы
-# 2. Получить список всех учеников в указанном классе
-#  (каждый ученик отображается в формате "Фамилия И.О.")
-# 3. Получить список всех предметов указанного ученика 
-#  (Ученик --> Класс --> Учителя --> Предметы)
-# 4. Узнать ФИО родителей указанного ученика
-# 5. Получить список всех Учителей, преподающих в указанном классе
+Побеждает тот, кто первый закроет все числа на своей карточке.
+Пример одного хода:
+Новый бочонок: 70 (осталось 76)
+------ Ваша карточка -----
+ 6  7          49    57 58
+   14 26     -    78    85
+23 33    38    48    71   
+--------------------------
+-- Карточка компьютера ---
+ 7 11     - 14    87      
+      16 49    55 77    88    
+   15 20     -       76  -
+--------------------------
+Зачеркнуть цифру? (y/n)
+Подсказка: каждый следующий случайный бочонок из мешка удобно получать 
+с помощью функции-генератора.
+Подсказка: для работы с псевдослучайными числами удобно использовать 
+модуль random: http://docs.python.org/3/library/random.html
+"""
+import random
+
+def get_numbers(n = 15, max = 90):
+    i = 1
+    cartlist = []
+    while i <= n:
+        number = random.randint(1, max)
+        if number not in cartlist:
+            cartlist.append(number)
+        else:
+            continue
+        i+=1
+    return cartlist
+
+def get_cards(cartlist):
+    i = 0
+    list = []
+    while i < 3:
+        list.append(cartlist[i*5:i*5 + 5])
+        list[i] = list[i] + ["  "] * 4
+        random.shuffle(list[i])
+        i+=1
+    return list
+
+def show_cards(list):
+    card = ""
+    for el1 in list.copy():
+        l = ""
+        for el2 in el1:
+            # переводим числа в строки, если число состоит из 1-й цифры то добавляем ей пробел
+            s = str(el2)
+            if len(s) == 1:
+                s = " " + s
+            l = l + " " + s
+        card = card + l + "\n"
+    return card
 
 
-print("HOMEWORK 6 normal")
-
-# class school:
-#     def __init__(self, number):
-#         self.number_scool = number
-#
-#     def classes(self):
-#         print("Вывод списка классов в школе № {}".format(self.number_scool))
-#
-#
-# class klass(school):
-#     def __init__(self, number):
-#         self.number_klass = number
-#
-#     def list_pupils(self):
-#         print("Список учеников класса {} в школе № {}".format(self.number_klass, self.number_scool))
+numbers_for_user = get_numbers()
+card_for_user = show_cards(get_cards(numbers_for_user))
+print('')
+print('------ Ваша карточка -----\n')
+print(card_for_user)
+print('--------------------------')
 
 
-class People:
-    def __init__(self, firstname, secondname, surname, school, klass):
-        self.fname = firstname
-        self.sname = secondname
-        self.surname = surname
-        self.school = school
-        self.klass = klass
-
-    def get_full_name(self):
-        return self.surname.title() + " " + self.fname.title() + ' ' + self.sname.title()
-
-    def get_short_name(self):
-        return self.surname.title() + " " + self.fname[:1].upper() + '. ' + self.sname[:1].upper() + "."
-
-    # def set_full_name(self, firstname, secondname, surname):
-    #     self.fname = firstname
-    #     self.sname = secondname
-    #     self.surname = surname
-
-# Ученики
-class Pupil(People):
-    def __init__(self, firstname, secondname, surname, school, klass, mother, father):
-        super().__init__(self, firstname, secondname, surname, school, klass)
-        self.mother = mother
-        self.farther = father
-
-    def show_fio(self):
-        print(self.get_full_name())
-
-    def show_parents(self):
-        print("Родители ученика {}: мать - {}, отец - {}".format(self.get_full_name(), self.mother, self.farther))
-
-# Учителя
-class Teacher(People):
-    def __init__(self, firstname, secondname, surname, school, klass, subject):
-        super().__init__(self, firstname, secondname, surname, school)
-        self.klass = klass.split(",")
-        self.subject = subject
-
-    def get_teacher_in_klass(self, klass_number):
-        if klass_number in self.klass:
-             print(self.get_full_name());
-
-
-
-pupils = [Pupil("Иван", "Иванович", "Иванов", '11', "4А", "Иванова А.А.", "Иванов Б.Б"),
-            Pupil("Петр", "Петрович" "Сидоров", '11', "5Б", "Сидорова А.А.", "Сидоров Б.Б."),
-            Pupil("Федор", "Федорович", "Петров", "11", "4В", "Петрова В.В.", "Петров С.С."),
-            ]
-
-teachers = [Teacher("Александр", "Иванович", "Иванов", '11', "4А,5Б,8В", "Математика"),
-            Teacher("Афанасий", "Петрович" "Сидоров", '11', "5Б,4В,7А", "Физика"),
-            Teacher("Антон", "Федорович", "Петров", "11", "8В,7А,9Б", "Химия"),
-            ]
+numbers_for_comp = get_numbers()
+card_for_comp = show_cards(get_cards(numbers_for_comp))
+print('')
+print('-- Карточка компьютера ---\n')
+print(card_for_comp)
+print('--------------------------')
